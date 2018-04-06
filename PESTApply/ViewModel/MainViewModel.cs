dfcee6a348816313d484846ce6c122d733e63d9f
孙兵 <carl900@live.com>
2018-04-06 19:24:58 +0800

    sync 安装及处理

--- PESTApply/ViewModel/MainViewModel.cs
+++ PESTApply/ViewModel/MainViewModel.cs
@@ -1,9 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Threading;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Navigation;
+
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Threading;
+using GalaSoft.MvvmLight.Messaging;
+using Newtonsoft.Json.Linq;
+using NPOI.SS.UserModel;
+using DoubleX.Infrastructure.Utility;
+using DoubleX.Infrastructure.DesktopUI;
+using DoubleX.Framework;
+using DoubleX.Module.Basics;
+using DoubleX.Module.PESTApply;
 
 namespace PESTApply
 {
     public class MainViewModel : ViewModelBase
     {
+        #region 公共属性
+
+        /// <summary>
+        /// 应用程序名称
+        /// </summary>
+        public string AppTitle
+        {
+            get { return appTitle; }
+            set { appTitle = value; RaisePropertyChanged(() => appTitle); }
+        }
+        private string appTitle = EngineHelper.GetConfig().AppTitle;
+
+
+        #endregion
+
         public MainViewModel()
         {
             ////if (IsInDesignMode)
